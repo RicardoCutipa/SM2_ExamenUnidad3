a188fd58177465f2ff1ae1f2cb0d5d752ccbad92
RicardoCutipa <rc2021069827@virtual.upt.pe>
2025-06-27 18:59:47 -0500

    2

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'services/location_service.dart';
 import 'services/notification_service.dart';
